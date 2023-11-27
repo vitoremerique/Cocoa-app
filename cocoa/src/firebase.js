4ac5c109de9c78cbd04ab336f1f92d8d43046def
Vitor Torres Emerique <62085257+vitoremerique@users.noreply.github.com>
2023-11-27 18:44:53 -0300

    Deleting credential firebase.js

--- cocoa/src/firebase.js
+++ cocoa/src/firebase.js
@@ -10,12 +10,7 @@
 
 // // Your web app's Firebase configuration
 // export const firebaseConfig = {
-//   apiKey: "AIzaSyDV7eA9UnHwf2NVDuxLY1FQiTKeZFNmWxo",
-//   authDomain: "cocoapp-4a6d7.firebaseapp.com",
-//   projectId: "cocoapp-4a6d7",
-//   storageBucket: "cocoapp-4a6d7.appspot.com",
-//   messagingSenderId: "773499089357",
-//   appId: "1:773499089357:web:c9d4f5f34f0a172a491484"
+//
 // };
 // /** 
 // *@param {*} uri
